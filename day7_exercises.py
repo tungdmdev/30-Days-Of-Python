d4c1dccd40c1fc131220eff7918e28d814c64e6d
tungdmdev <tungdm.dev@gmail.com>
2025-10-08 16:16:24 +0700

    Update: Day 7 done

--- day7_exercises.py
+++ day7_exercises.py
@@ -34,3 +34,4 @@ age_set = set(age)
 # I am a teacher and I love to inspire and teach people. How many unique words have been used in the sentence? Use the split methods and set to get the unique words.
 sentence = "I am a teacher and I love to inspire and teach people"
 words = set(sentence.split(''))
+print(words)
